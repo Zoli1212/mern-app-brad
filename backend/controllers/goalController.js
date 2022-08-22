6660d353fed23ca8b57cb7aceb0cd3e841ff2db0
Your Name <you@example.com>
2022-08-22 19:04:56 +0200

    fourth

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -2,6 +2,7 @@ const asyncHandler = require('express-async-handler')
 
 
 const Goal = require('../models/goalModel')
+const User = require('../models/userModel')
 
 const getGoals = asyncHandler(async (req, res) =>{
 
@@ -36,6 +37,22 @@ const updateGoal = asyncHandler(async (req, res) => {
         throw new Error('Goal not found')
     }
 
+    const user = await User.findById(req.user.id)
+
+    if(!user){
+
+        res.status(401)
+        throw new Error('User not found')
+
+    }
+
+    if(goal.user.toString() !== user.id){
+
+        res.status(401)
+        throw new Error('user not authorized')
+
+    }
+
     const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, { new: true})
     res.status(200).json(updatedGoal)
 
@@ -50,6 +67,11 @@ const deleteGoal = asyncHandler(async (req, res) =>{
         res.status(400)
         throw new Error('Goal not found')
     }
+
+    if (goal.user.toString() !== req.user.id) {
+        res.status(401)
+        throw new Error('User not authorized')
+      }
 
     const removedGoal = await goal.remove()
 
