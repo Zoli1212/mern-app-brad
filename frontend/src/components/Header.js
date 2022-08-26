6be1b628e8e0777e5b922251f749716a56f0b97d
Your Name <you@example.com>
2022-08-26 19:17:59 +0200

    fourth

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -2,7 +2,24 @@ import React from 'react'
 import { FaSignInAlt, FaSignOutAlt, FaUser } from 'react-icons/fa'
 import { Link } from 'react-router-dom'
 
+import { useSelector, useDispatch} from 'react-redux'
+import { logout, reset } from '../features/auth/authSlice'
+import { useNavigate } from 'react-router-dom'
+
 export const Header = () => {
+
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+    const { user} = useSelector((state) => state.auth)
+
+
+    const onLogout = () => {
+
+        dispatch(logout())
+        dispatch(reset())
+        navigate('/')
+
+    }
   return (
     <header className='header'>
         <div className="logo">
@@ -10,6 +27,19 @@ export const Header = () => {
         </div>
 
         <ul>
+
+            { user ? <>
+            <li>
+                <button className='btn' onClick={onLogout}>
+                    <FaSignOutAlt /> Logout
+                </button>
+
+                
+
+            </li>
+            
+            
+            </>:<>
             <li>
                 <Link to='/login'>
                     <FaSignInAlt /> Login
@@ -22,8 +52,10 @@ export const Header = () => {
                     <FaUser /> Register
 
                 </Link>
-
             </li>
+            </>
+            }
+
 
         </ul>
     </header>
